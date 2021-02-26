f2830fec3a026e1d8e03b3314964648ed3dbaea4
unknown <christinragy@yahoo.com>
2021-02-26 18:21:59 +0200

    adjusting the fn logic

--- Application/apps.py
+++ Application/apps.py
@@ -9,7 +9,7 @@ loan_amount =5000
 Interest_Rate =15
 fees = 3
 loan_period = 0
-total_amount =0
+total_amount = fees
 account_amount= 12000
 
 
@@ -21,10 +21,11 @@ def check_balance(account_amount, loan_amount, fees):
 		
 def submit_loan(account_amount, loan_amount, Interest_Rate):
 	if(check_balance(account_amount, loan_amount,fees)):
-		loan_period =loan_period + 1
-		total_amount = total_amount+ loan_amount(1+Interest_Rate/600) + fees
-		if (loan_period == 6):
-			loan_status= "Completed"
+            while(loan_period <= 6) :
+                loan_period = loan_period + 1
+                total_amount = total_amount+ loan_amount(1+Interest_Rate/600)
+                if (loan_period == 6):
+                    loan_status= "Completed"
 			
 if __name__ == "__main__":
     submit_loan(account_amount, loan_amount, Interest_Rate)
